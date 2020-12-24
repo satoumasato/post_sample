c564e34c0cfffe343f306b92cc1e8b1f80865085
satoumasato <masakun1993@gmail.com>
2020-12-24 17:40:44 +0900

    詳細表示を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,29 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def show
   end
 
   def new
+    @post = Post.new
   end
 
-  
+  def create
+    post = Post.create!(post_params)
+    redirect_to post
+  end
+  def show
+
+  @post = Post.find(params[:id])
+end
+
 
   def adit
+  end
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 
 
