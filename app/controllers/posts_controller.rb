48e857d535881e2696ec38cd575187445f4eea46
satoumasato <masakun1993@gmail.com>
2020-12-24 21:52:22 +0900

    編集を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,12 +21,23 @@ end
   def destroy
     post = Post.find(params[:id])
     post.destroy!
-    
+
   end
 
 
-  def adit
-  end
+  def edit
+   # ***** 以下を追加 *****
+   @post = Post.find(params[:id])
+   # ***** 以上を追加 *****
+ end
+
+ def update
+   # ***** 以下を追加 *****
+   post = Post.find(params[:id])
+   post.update!(post_params)
+   
+ end
+
   def post_params
     params.require(:post).permit(:title, :content)
   end
